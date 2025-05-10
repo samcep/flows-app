a2507d44f4ede5fef43cf9f9be35d9922352258a
Samuel Casalla <samuelcepeda301@gmail.com>
2025-05-10 14:04:06 -0500

    feat: add repositories for all entities

---
+++ flows-app/Repositories/FieldStepRepository.cs
@@ -0,0 +1,12 @@
+﻿using flows_app.Entities;
+using flows_app.Interfaces;
+
+namespace flows_app.Services
+{
+    public interface IFlowStepRepository : IAsyncRepository<FlowStep> { }
+    public class FlowStepRepository : AsyncRepository<FlowStep>, IFlowStepRepository
+    {
+        public FlowStepRepository(ApplicationDbContext dbContext) : base(dbContext) { }
+    }
+
+}
---
+++ flows-app/Repositories/StepRepository.cs
@@ -0,0 +1,11 @@
+﻿using flows_app.Entities;
+using flows_app.Interfaces;
+
+namespace flows_app.Services
+{
+    public interface IStepRepository : IAsyncRepository<Step> { }
+    public class StepRepository : AsyncRepository<Step>, IStepRepository
+    {
+        public StepRepository(ApplicationDbContext dbContext) : base(dbContext) { }
+    }
+}
